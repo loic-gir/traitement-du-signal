d9dae69e42ed3c242a50a586beb64ad79bc5d1f5
Loïc <116891483+loic-gir@users.noreply.github.com>
2025-11-17 18:13:19 +0100

    Refactor Rob function to output yrob instead of rob

--- Rob.m
+++ Rob.m
@@ -1,11 +1,4 @@
 function yrob = Rob(y,fc,Fs)
-% Rob : Fonction de robotisation de la voix
-%
-% y : signal audio d'entree (vecteur colonne)
-% fc : frequence de la porteuse en Hz (200, 500, 1000, 2000)
-% Fs : frequence d'echantillonnage
-%
-% yrob : signal audio "robotise" en sortie
 
 % Nombre d'echantillons du signal
 N = length(y);
@@ -14,13 +7,12 @@ N = length(y);
 t = (0:N-1)' / Fs;  % Vecteur colonne pour correspondre a y
 
 % Modulation par une exponentielle complexe a la frequence fc
-% yrob(t) = y(t) * exp(j*2*pi*fc*t)
 signal = y .* exp(1i * 2 * pi * fc * t);
 
 % On recupere la partie reelle du signal module
-rob = real(signal);
+yrob = real(signal);
 
 % Normalisation pour eviter la saturation
-rob = rob / max(abs(rob));
+yrob = yrob / max(abs(yrob));
 
 end
