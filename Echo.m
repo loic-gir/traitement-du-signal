dfddd2c98ec4853c9dd30d17d069bea90a834854
Loïc <116891483+loic-gir@users.noreply.github.com>
2025-11-24 13:20:19 +0100

    Rename parameters for echo function

--- Echo.m
+++ Echo.m
@@ -1,11 +1,11 @@
-function y_echo = Echo(y, Fs, delay_ms, decay, num_echos)
+function y_echo = Echo(y, Fs, delay_ms, att, nb_echos)
 % Echo : Ajoute un effet d'écho au signal audio
 %
 % y         : signal d'entrée
 % Fs        : fréquence d'échantillonnage
 % delay_ms  : délai de l'écho en millisecondes
-% decay     : facteur d'atténuation (0 à 1)
-% num_echos : nombre d'échos successifs
+% att     : facteur d'atténuation (0 à 1)
+% nb_echos : nombre d'échos successifs
 %
 % y_echo    : signal avec écho
 
@@ -13,17 +13,17 @@ function y_echo = Echo(y, Fs, delay_ms, decay, num_echos)
 delay_samples = round(delay_ms * Fs / 1000);
 
 % Longueur du signal de sortie
-output_length = length(y) + num_echos * delay_samples;
+output_length = length(y) + nb_echos * delay_samples;
 y_echo = zeros(output_length, 1);
 
 % Signal original
 y_echo(1:length(y)) = y;
 
 % Ajout des échos successifs
-for i = 1:num_echos
+for i = 1:nb_echos
     start_idx = i * delay_samples + 1;
     end_idx = start_idx + length(y) - 1;
-    attenuation = decay^i;
+    attenuation = att^i;
     y_echo(start_idx:end_idx) = y_echo(start_idx:end_idx) + attenuation * y;
 end
 
